5975bcbcba6477bc07df4f4c9ec02c2b83126c8b
ddpanda <itsddpanda@outlook.com>
2025-03-13 19:44:12 +0530

    dev deployed

--- main.py
+++ main.py
@@ -103,4 +103,4 @@ app.add_middleware(AuthLoggingMiddleware)
 app.add_middleware(RateLimitMiddleware)  
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8100, server_header=False) #true for dev env
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, server_header=False) #true for dev env
